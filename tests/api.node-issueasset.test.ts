62b7c0c7e54beba7f0ffe45bc88b48aac0b4500f
Yurii Bandrivskyi <yuriibandrivskyi@MacBook-Pro.local>
2025-03-17 21:56:29 +0200

    UPD

--- tests/api.node-issueasset.test.ts
+++ tests/api.node-issueasset.test.ts
@@ -11,6 +11,8 @@ const NODE_NAME = process.env.NODE_NAME;
 const NODE_ID = process.env[`${NODE_NAME}_ID`];
 
 test.describe.serial('Create Asset', () => {
+    console.log(NODE_NAME);
+    console.log(process.env);
     const assetAmount = 1000;
     const assetName = 'USDT';
     let asset_id = '';
@@ -25,8 +27,10 @@ test.describe.serial('Create Asset', () => {
 
     test('get Node', async ({ request }) => {
         const { data } = await getNode(request, NODE_ID);
+        console.log(data);
         const { invoke_url } = data;
         nodeApi = invoke_url;
+        invoke_url.test
     });
 
     test('call if /createutxos', async ({ request }) => {
